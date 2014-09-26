188c29333bfe439a49da0286108acc1e6c670074
Ricky Egeland <ricky.egeland@gmail.com>
2014-09-26 16:56:55 -0600

    Add timing functions for performance testing

--- sunstardb/utils.py
+++ sunstardb/utils.py
@@ -12,3 +12,24 @@ def compress_space(s):
 def wikiword(s):
     """Turn a string into a word suitable for using in a wiki URL"""
     return re.sub(r'[^\w-]', ' ', s).title().replace(' ', '')
+
+FIRST_TIME = None
+LAST_TIME = None
+def time_reset():
+    global FIRST_TIME
+    global LAST_TIME
+    FIRST_TIME = datetime.datetime.now()
+    LAST_TIME = FIRST_TIME
+time_reset() # set the globals on module load
+
+def time_lap():
+    global LAST_TIME
+    t = datetime.datetime.now()
+    elapsed = (t - LAST_TIME).total_seconds()
+    LAST_TIME = t
+    return elapsed
+
+def time_total():
+    t = datetime.datetime.now()
+    elapsed = (t - FIRST_TIME).total_seconds()
+    return elapsed
